7829c55b6fcdd37e1c88f446233d9dafc72bb914
Sanjay Tailor <st.sanjaytailor@gmail.com>
2018-02-24 15:34:45 +0530

    Now the user can be seen... working correctly

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {NavController} from "ionic-angular";
+import {UserPage} from "./User/user";
 
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
 export class UsersPage {
+  constructor(private navCtrl: NavController) {}
+
+  onLoadUser(name: string){
+    this.navCtrl.push(UserPage, {userName: name});
+  }
 }
